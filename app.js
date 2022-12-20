953df8dbf66fd7b1c5ca0c541d0b814fe3416d8c
Aulugbekov <119114287+Aulugbekov@users.noreply.github.com>
2022-12-20 07:37:42 +0300

    Update app.js
    new

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-let tags = {
-  li: 2,
-  a: 3,
-  div: 4,
-  span: 6,
-  h3: 8,
-};
-let target = {};
-Object.assign(target, tags);
+let arr = [
+  "li",
+  "span",
+  "div",
+  "span",
+  "h3",
+  "a",
+  "div",
+  "span",
+  "a",
+  "li",
+  "div",
+  "div",
+];
+let li = 0;
+let h3 = 0;
+let a = 0;
+let div = 0;
+let span = 0;
 
-console.log(tags);
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == "li") {
+    li += 1;
+  } else if (arr[i] == "h3") {
+    h3 += 1;
+  } else if (arr[i] == "a") {
+    a += 1;
+  } else if (arr[i] == "div") {
+    div += 1;
+  } else if (arr[i] == "span") {
+    span += 1;
+  }
+}
+
+console.log({
+  a: a,
+  span: span,
+  h3: h3,
+  li: li,
+  div: div,
+});
